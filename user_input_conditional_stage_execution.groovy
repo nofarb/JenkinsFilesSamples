d5b7b9734da0c1aa541fb71dee6baebb805a6ca7
nofarb <nofarb@gmail.com>
2019-01-24 08:13:16 -0500

    added comments

--- user_input_conditional_stage_execution.groovy
+++ user_input_conditional_stage_execution.groovy
@@ -20,18 +20,17 @@ pipeline{
                 timeout(time: 1, unit: 'HOURS')  //if user input not given in 1 hour stage will fail. not mandatory to set TO
             }   
             when{
-                environment name: 'SHOULD_PROMOTE', value: 'YES' 
+                environment name: 'SHOULD_PROMOTE', value: 'YES' //Execute stage if value of "SHOULD_PROMOTE" is "YES"
             }
          
-            input {
+            input { //propmt the user for value for "SHOULD_PROMOTE" parameter
                 message "Should we promote to PROD?"
                 ok "submit patameter value"
                 parameters {
                     string(name: 'SHOULD_PROMOTE', defaultValue: 'NO', description: 'Should we promote build to PRODUCTION? YES/NO')
                 }
             }
-            steps {
-                echo "Paramter 'SHOULD_PROMOTE' Result: [${SHOULD_PROMOTE}]"
+            steps { 
                 echo 'App deployed to PROD'
 
             }
